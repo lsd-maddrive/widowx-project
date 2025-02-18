a63e8706a6394b0f01c98bba10158649c4faed8a
Nikita Lobazev <nlobazev@gmail.com>
2025-02-18 13:33:50 +0300

    moved env setup in ros shared

--- scripts/install_binary_packages.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo apt-get update && sudo apt-get install -y \
-    ros-$ROS_DISTRO-moveit \
-    ros-$ROS_DISTRO-dynamixel-sdk \
-    ros-$ROS_DISTRO-dynamixel-workbench \
-    ros-$ROS_DISTRO-roboticsgroup-upatras-gazebo-plugins
